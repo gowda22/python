3dd7b29a85821402c42c01fc54d2bf83f07b1e88
Gowda <gowdavasista22@gmail.com>
2026-08-29 19:56:11 +0530

    Add Python practice files

---
+++ WHILE_LOOP.py
@@ -0,0 +1,36 @@
+#WHILE{Print blk st untill con true}
+
+# i=10#initialize 
+# while i>=1: #condition
+#     print(i)#work
+    
+#     i-=1 # update
+
+# print("execution completed")
+
+
+#MUL
+# # MUL
+# num = int(input("Enter the num: "))
+# i = 1
+
+# while i <= 10:
+#     print(num, "x", i, "=", num * i)
+#     i += 1
+
+#even 
+# num = int(input("Enter the number: "))
+
+# i = 0
+
+# while i <= num:
+#     if i % 2 == 0:
+#         print(i)
+#     i += 1 #inc 2 i+=2
+
+
+# while i <= num:
+#     print(i)
+#     i += 2
+
+
---
+++ sting_functions.py
@@ -0,0 +1,54 @@
+#lenth 
+string1=("good morninggg")
+# print(len(string1))# len of sting
+
+
+#lower
+print(string1.lower())
+
+
+#upper
+print(string1.upper())
+
+
+
+#capitalize
+
+print(string1.capitalize())
+
+
+#title
+
+print(string1.title())
+
+
+#FIND
+
+print(string1.find())
+
+#replace
+print(string1.replace("morninggg","evening"))
+
+
+#count()
+
+print(string1.count())
+
+# index
+
+print(string1.index())
+
+
+
+
+
+
+
+#strip  
+
+srt1="     good morning    "
+print(len(srt1))
+str2=srt1.strip()
+print(srt1)
+print(str2)
+print(len(str2))
